13f75f10f97a29912945ea5467e7bf5c31b874b5
Letícia Aguiar <letsaguiar@yandex.com>
2024-11-11 19:28:03 -0300

    feat: setup signal handler

--- server/server.c
+++ server/server.c
@@ -1,7 +1,16 @@
-#include "stdio.h"
+#include <stdio.h>
+#include <signal.h>
+#include <stdlib.h>
+
+void handle_signal(int sig)
+{
+    printf("Caught signal %d\n", sig);
+    exit(0);
+}
 
 int main(void)
 {
-    printf("Hello, World!\n");
-    return (0);
+    signal(SIGINT, handle_signal);
+    while (1)
+        ;
 }
