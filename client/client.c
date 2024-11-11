1899bd4ec21e0cec40da8ab0a3b709f326f99cd4
Letícia Aguiar <letsaguiar@yandex.com>
2024-11-11 19:58:41 -0300

    feat: setup client

--- client/client.c
+++ client/client.c
@@ -1,6 +1,42 @@
 #include "libft.h"
+#include "ft_printf.h"
+#include <signal.h>
+#include <unistd.h>
 
-int main(void)
+static void send_message(int server_pid, t_string message)
 {
-    ft_putstr_fd("Client PID: ", 1);
+    int i = 0;
+    while (message[i])
+    {
+        int j = 0;
+        while (j < 8)
+        {
+            if (message[i] & (1 << j))
+                kill(server_pid, SIGUSR1);
+            else
+                kill(server_pid, SIGUSR2);
+            j++;
+            usleep(100);
+        }
+        i++;
+    }
+}
+
+int main(int argc, char **argv)
+{
+    if (argc != 3)
+    {
+        ft_putstr_fd("Usage: ./client [server-pid] [message]\n", 1);
+        return (1);
+    }
+
+    int server_pid = ft_atoi(argv[1]);
+    if (!server_pid)
+    {
+        ft_putstr_fd("Invalid server PID\n", 1);
+        return (1);
+    }
+
+    t_string message = argv[2];
+    send_message(server_pid, message);
 }
