98f45612bb284323faf1684dcaa2f9627c42171e
Letícia Aguiar <letsaguiar@yandex.com>
2024-11-11 19:32:48 -0300

    feat: add libft dependencie

--- run.sh
+++ run.sh
@@ -3,10 +3,12 @@
 # Compile
 export CC="gcc"
 export CFLAGS="-Wall -Wextra -Werror -g -fsanitize=address"
+export CLIBS="-Llibs -lft"
+export CINCLUDES="-Iincludes"
 
 export SERVER_FILES=$(find server -name "*.c")
 export SERVER="server.out"
-$CC $CFLAGS $SERVER_FILES -o $SERVER
+$CC $CFLAGS $SERVER_FILES -o $SERVER $CINCLUDES $CLIBS
 
 # Run Server
 ./$SERVER
--- server/server.c
+++ server/server.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <signal.h>
 #include <stdlib.h>
+#include "libft.h"
 
 void handle_signal(int sig)
 {
-    printf("Caught signal %d\n", sig);
+    printf("Caught signal %s\n", ft_itoa(sig));
     exit(0);
 }
 
